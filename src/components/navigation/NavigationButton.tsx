0720347edc9a0db8840373de113b047202732e1b
hyunn522 <tjgus52213@gmail.com>
2025-08-20 00:31:37 +0900

    feat: react-icons 적용

--- src/components/navigation/NavigationButton.tsx
+++ src/components/navigation/NavigationButton.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import type { IconType } from 'react-icons';
 
 interface Props {
+  Icon: IconType;
   title: string;
   isActive: boolean;
   onClick: () => void;
 }
 
-const NavigationButton: React.FC<Props> = ({ title, isActive, onClick }) => {
+const NavigationButton: React.FC<Props> = ({ Icon, title, isActive, onClick }) => {
+  const activeColor = '#606AB1';
+  const inactiveColor = 'black';
+
   return (
     <div className="flex flex-col justify-center items-center cursor-pointer" onClick={onClick}>
+      <Icon size={24} color={isActive ? activeColor : inactiveColor} />
       <div className={`text-nav mt-1 ${isActive ? 'text-[#606AB1]' : 'text-black'}`}>{title}</div>
     </div>
   );
