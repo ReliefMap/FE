54e4ac3b2d03093856a579b2494fab38a03c6d68
hyunn522 <tjgus52213@gmail.com>
2025-08-31 18:15:33 +0900

    feat: 물품 목록 화면에 날짜 및 시간 선택 기능 추가

--- src/pages/Goods/index.tsx
+++ src/pages/Goods/index.tsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react';
 import GoodsItem from '../../components/goods/GoodsItem';
-import type { GoodsItemType } from '../../types/Goods';
+import type { GoodsItemType, TimeProps } from '../../types/Goods';
+import DateBar from '../../components/goods/DateBar';
+import DateSheet from '../../components/goods/DateSheet';
 
 const Goods = () => {
   const [goodsList, setGoodsList] = useState<GoodsItemType[]>([]);
+  const [time, setTime] = useState<TimeProps>({});
+  const [sheetOpen, setSheetOpen] = useState(false);
 
   useEffect(() => {
-    // TODO: API 연결 후 수정 예정
+    /* TODO: API 연결 후 수정 예정 */
     setGoodsList([
       { id: 1, name: '신라면', image: '/shinramen.png', stock: 10 },
       { id: 2, name: '물', image: '/water.png', stock: 5 },
     ]);
   }, []);
 
-  // 예약하기 클릭
   const handleReserve = async (id: number) => {
     try {
       alert(`${id}번 물품 예약 완료!`);
@@ -24,18 +27,27 @@ const Goods = () => {
   };
 
   return (
-    <div className="p-4">
-      {goodsList.map((goods) => (
-        <GoodsItem
-          key={goods.id}
-          id={goods.id}
-          name={goods.name}
-          image={goods.image}
-          stock={goods.stock}
-          onReserve={handleReserve}
-        />
-      ))}
-    </div>
+    <>
+      <DateBar value={time} onOpen={() => setSheetOpen(true)} onClear={() => setTime({})} />
+      <div className="p-4">
+        {goodsList.map((goods) => (
+          <GoodsItem
+            key={goods.id}
+            id={goods.id}
+            name={goods.name}
+            image={goods.image}
+            stock={goods.stock}
+            onReserve={handleReserve}
+          />
+        ))}
+      </div>
+      <DateSheet
+        open={sheetOpen}
+        initial={time}
+        onClose={() => setSheetOpen(false)}
+        onApply={(t: TimeProps) => setTime(t)}
+      />
+    </>
   );
 };
 
